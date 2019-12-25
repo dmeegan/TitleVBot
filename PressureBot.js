f9e84c86c6eb8e73d04a35c2e1dfcf177df2a344
dmeegan <dn_meegan@yahoo.com>
2019-12-25 13:33:10 -0500

    I revised some of the variable names to be more specific to which part of the system that they are being used for.

--- PressureBot.js
+++ PressureBot.js
@@ -36,21 +36,21 @@ const latTargetHeadloss = () => 0.21 * distalHead
 let latSizes = [1, 1.25, 1.5, 2, 3, 4]
 
 let latDiaIterator = 0
-let deltaHead = 0
+let latDeltaHead = 0
 do {
     let perfFlowIterator = 0
-    let sumHead = 0
+    let latSumHeadLoss = 0
     do {
        const downStreamFlow = () => perfDis() * (perfNum()-perfFlowIterator)
        const latHeadLoss = () => perfSpacing * Math.pow((3.55*downStreamFlow()) / (roughCoeff * (Math.pow((latSizes[latDiaIterator]),2.63))),1.85)
-       sumHead += latHeadLoss()
+       latSumHeadLoss += latHeadLoss()
         perfFlowIterator++
         } while (perfFlowIterator < (perfNum()+1))
-    const pressureEndHeadLoss = () => perfSpacing * Math.pow((3.55*perfDis()*perfNum()) / (roughCoeff * (Math.pow((latSizes[latDiaIterator]),2.63))),1.85)
-    deltaHead = sumHead - pressureEndHeadLoss()
+    const latPressureEndHeadLoss = () => perfSpacing * Math.pow((3.55*perfDis()*perfNum()) / (roughCoeff * (Math.pow((latSizes[latDiaIterator]),2.63))),1.85)
+    latDeltaHead = latSumHeadLoss - latPressureEndHeadLoss()
     latDia = latSizes[latDiaIterator]
     latDiaIterator++
-} while (deltaHead > latTargetHeadloss())
+} while (latDeltaHead > latTargetHeadloss())
 
 
 // let perfFlowIterator = 0
