79ae7b284c14eca213c59b4573a0c11789c4df59
dmeegan <dn_meegan@yahoo.com>
2020-04-08 20:01:24 -0400

    Fixed alert for trench height validation

--- TitleVBotFormValidation.js
+++ TitleVBotFormValidation.js
@@ -44,10 +44,10 @@ function validateTrenchWidth() {
 function validateTrenchHeight() {
   var trenchHeightValue = +document.getElementById('UserInput-trenchHeight').value
   if (trenchHeightValue < 0.5) {
-    alert("Invalid Input: Per Title V section 15.251, the minimum trench width shall be 0.5 feet");
+    alert("Invalid Input: Per Title V section 15.251, the minimum trench height shall be 0.5 feet");
     document.getElementById('UserInput-trenchHeight').value = 0.5
   } else if (trenchHeightValue > 2) {
-    alert("Invalid Input: Per Title V section 15.251, the maximum trench width shall be 2 feet");
+    alert("Invalid Input: Per Title V section 15.251, the maximum trench height shall be 2 feet");
     document.getElementById('UserInput-trenchHeight').value = 2
   }
 }
